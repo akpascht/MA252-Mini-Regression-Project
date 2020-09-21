cbdbde38935df40154d0e13cebaf589ef91776db
akpascht <68290484+akpascht@users.noreply.github.com>
2020-09-21 02:29:46 -0400

    Add files via upload

--- Part 3 Transformation R Script.R
+++ Part 3 Transformation R Script.R
@@ -4,16 +4,23 @@
 #natural log (+1) of price
 lnprice<-log(kc_house_data$price + 1)
 hist(lnprice)
+boxplot(lnprice)
+
+#sqft living vs ln price
+summary(lm(lnprice~kc_house_data$sqft_living))
+plot(lm(lnprice~kc_house_data$sqft_living))
+plot(kc_house_data$sqft_living,lnprice)
 
 #natural log of sqft living
 lnsqft<-log(kc_house_data$sqft_living + 1)
 hist(lnsqft)
+boxplot(lnsqft)
 
 #regression, ln price vs ln sqft living
 plot(lnsqft,lnprice) #looks good
 summary(lm(lnprice~lnsqft)) 
 plot(lm(lnprice~lnsqft))  #qq plot looks really good
-
+plot(lnsqft,lnprice)
 
 #price vs sqft_living15
 lnsqft15<-log(kc_house_data$sqft_living15 + 1)
@@ -30,6 +37,12 @@ summary(lm(lnprice~kc_house_data$zipcode))
 plot(lm(lnprice~kc_house_data$zipcode))
 
 #price vs bathrooms 
+lnbathrooms<-log(kc_house_data$bathrooms +1)
+summary(lm(lnprice~lnbathrooms))
+plot(lnbathrooms,lnprice)
+hist(lnbathrooms)
+
+
 plot(kc_house_data$bathrooms,lnprice)
 summary(lm(lnprice~kc_house_data$bathrooms))
 plot(lm(lnprice~kc_house_data$bathrooms))
